b581f8034eca99efe44fd36f3e08b8bc536ba813
sekihan <sekihan0291@gmail.com>
2018-07-22 12:29:45 +0900

    no message

---
+++ text/ZipDownload.py
@@ -0,0 +1,22 @@
+# utf-8
+"""
+青空文庫に公開中の作品を作家別に一括ダウンロードできるサイト
+http://keison.sakura.ne.jp/index.html
+から夏目漱石の作品一覧をダウンロードする。
+"""
+import os.path, urllib.request as req
+import re, zipfile, sys
+
+# 夏目漱石の作品一覧のダウンロード
+local = 'natume.zip'
+url = 'http://keison.sakura.ne.jp/nagyou/natume.zip'
+
+if not os.path.exists(local):
+    print('開始')
+    req.urlretrieve(url, local)
+    print('終了')
+
+# ZIPファイルの中のテキストファイルを読む
+# z = zipfile.ZipFile(local, 'r') # zipファイルを読み込む
+# 展開する
+# z.extractall(path=os.path.dirname(local))
